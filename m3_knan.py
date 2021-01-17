59eaf95033c11d53d045d68024cb3a5acf552254
dinhthong <nguyendinhthongit@gmail.com>
2021-01-17 14:33:31 +0700

    create new table and show all tables in the database, ok

--- m3_knan.py
+++ m3_knan.py
@@ -1,13 +1,39 @@
-from getpass import getpass
-from mysql.connector import connect, Error
+import mysql.connector
 
-try:
-    with connect(
-        host="localhost",
-        user=input("Enter username: "),
-        password="cxzzxcCC",
-        database="m3_knan",
-    ) as connection:
-        print(connection)
-except Error as e:
-    print(e)
+conn = mysql.connector.connect(
+         user='root',
+         password='cxzzxcCC',
+         host='127.0.0.1',
+         database='m3_knan')
+
+# create_movies_table_query = """
+# CREATE TABLE movies(
+#     id INT AUTO_INCREMENT PRIMARY KEY,
+#     title VARCHAR(100),
+#     release_year YEAR(4),
+#     genre VARCHAR(100),
+#     collection_in_mil INT
+# )
+# """
+
+create_movies_table_query = """
+CREATE TABLE movies(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    release_year YEAR(4),
+    genre VARCHAR(100),
+    collection_in_mil INT
+)
+"""
+with conn.cursor() as cursor:
+    cursor.execute(create_movies_table_query)
+    conn.commit()
+
+
+show_db_query = "SHOW TABLES"
+with conn.cursor() as cursor:
+    cursor.execute(show_db_query)
+    for db in cursor:
+        print(db)
+        
+conn.close()
