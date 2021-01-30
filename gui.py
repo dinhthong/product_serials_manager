a6f37006d077b385d4080ecbf8ef25fb8a2f54a2
dinhthong <nguyendinhthongit@gmail.com>
2021-01-30 11:50:45 +0700

    gui app demo, login to database

---
+++ gui.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+# import Tkinter
+# top = Tkinter.Tk()
+# # Code to add widgets will go here...
+# top.mainloop()
+
+#30Jan21 11h50 login to database url: https://www.geeksforgeeks.org/create-mysql-database-login-page-in-python-using-tkinter/
+import Tkinter as tk
+import mysql.connector 
+from Tkinter import *
+  
+ 
+def submitact():
+     
+    user = Username.get()
+    passw = password.get()
+  
+ #   print(f"The name entered by you is {user} {passw}")
+  
+    logintodb(user, passw)
+  
+ 
+def logintodb(user, passw):
+     
+    # If paswword is enetered by the 
+    # user
+    if passw:
+        db = mysql.connector.connect(host ="localhost",
+                                     user = user,
+                                     password = passw,
+                                     db ="m3_knan")
+        cursor = db.cursor()
+         
+    # If no password is enetered by the
+    # user
+    else:
+        db = mysql.connector.connect(host ="localhost",
+                                     user = user,
+                                     db ="College")
+        cursor = db.cursor()
+         
+    # A Table in the database
+    savequery = "select * from movies"
+     
+    try:
+        cursor.execute(savequery)
+        myresult = cursor.fetchall()
+         
+        # Printing the result of the
+        # query
+        for x in myresult:
+            print(x)
+        print("Query Excecuted successfully")
+         
+    except:
+        db.rollback()
+        print("Error occured")
+  
+ 
+root = tk.Tk()
+root.geometry("300x300")
+root.title("DBMS Login Page")
+  
+ 
+# Definging the first row
+lblfrstrow = tk.Label(root, text ="Username -", )
+lblfrstrow.place(x = 50, y = 20)
+ 
+Username = tk.Entry(root, width = 35)
+Username.place(x = 150, y = 20, width = 100)
+  
+lblsecrow = tk.Label(root, text ="Password -")
+lblsecrow.place(x = 50, y = 50)
+ 
+password = tk.Entry(root, width = 35)
+password.place(x = 150, y = 50, width = 100)
+ 
+submitbtn = tk.Button(root, text ="Login", 
+                      bg ='blue', command = submitact)
+submitbtn.place(x = 150, y = 135, width = 55)
+ 
+root.mainloop()
